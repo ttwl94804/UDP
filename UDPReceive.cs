dbb4764d9e474225761d5237823f82d431d85e52
ttwl94804 <48583178+ttwl94804@users.noreply.github.com>
2019-06-14 15:15:56 +0800

    Add files via upload

---
+++ UDPReceive.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using UnityEngine;
+
+
+    public class UDPReceive : MonoBehaviour
+    {
+        [SerializeField]
+        private int port = 4566;   //端口 
+
+        [Tooltip("接收的包为文本")] public bool receiveAsText = true; //接收的包为文本
+        [Tooltip("接收的包为十六进制")] public bool receiveAsHex = true; //接收的包为十六进制
+        [Tooltip("在画面打印接收的包")] public bool showReceivePacket = true;    //在屏幕打印接受的数据包
+
+        private string received = "";
+        private UdpClient client;
+        private Thread receiveThread;
+
+        private Queue<byte[]> recieveQueue = new Queue<byte[]>();
+
+        public Action<byte[]> ReceiveEventHandler = new Action<byte[]>((packet) => { });
+
+        private string hexPrint = "", textPrint = "";   //用于GUI打印消息
+        public static UDPReceive instance;
+
+        public void Awake()
+        {
+
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+            client = new UdpClient(port);
+            receiveThread = new Thread(new ThreadStart(ReceiveData));
+            receiveThread.IsBackground = true;
+            receiveThread.Start();
+        }
+
+
+        void Update()
+        {
+            while (recieveQueue.Count > 0)
+            {
+                ReceiveEventHandler(recieveQueue.Dequeue());
+            }
+        
+        }
+
+
+        public void ReceiveData()
+        {
+            while (true)
+            {
+                try
+                {
+                    IPEndPoint anyIP = new IPEndPoint(IPAddress.Any, 0);
+                    byte[] data = client.Receive(ref anyIP);
+                    recieveQueue.Enqueue(data);
+                    if (showReceivePacket)
+                    {
+                        hexPrint = BitConverter.ToString(data);
+                        textPrint = Encoding.UTF8.GetString(data);
+                    }                  
+                }
+                catch (Exception err)
+                {
+                    Debug.Log("错误:" + err.ToString());
+                }
+            }
+        }
+
+        public void OnDisable()
+        {
+            if (receiveThread != null)
+            {
+                receiveThread.Abort();
+                receiveThread = null;
+            }
+            client.Close();
+            Debug.Log("UDPClient退出");
+        }
+
+        private void OnGUI()
+        {
+            if (!showReceivePacket) return;
+            GUI.Label(new Rect(0, 0, Screen.width, 500), "<size=30>Hex  : " + hexPrint + "</size>");
+            GUI.Label(new Rect(0, 50, Screen.width, 500), "<size=30>Text : " + textPrint + "</size>");
+        }
+
+
+    }
+
---
+++ UDPSend.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using UnityEngine;
+
+public class UDPSend : MonoBehaviour
+{
+    [SerializeField] [Tooltip("如果不填写Ip则广播")] private string ipSend = "";
+    [SerializeField] private int sendPort = 4566; //发送到的端口
+
+    private IPEndPoint remoteEndPoint;
+    private UdpClient client;
+    public static UDPSend instance;
+
+    public void Awake()
+    {
+        instance = this;
+        DontDestroyOnLoad(gameObject);
+
+        //如果不填写Ip则广播
+        IPAddress ip;
+        if (IPAddress.TryParse(ipSend, out ip))
+            remoteEndPoint = new IPEndPoint(ip, sendPort);
+        else
+            remoteEndPoint = new IPEndPoint(IPAddress.Broadcast, sendPort);
+        client = new UdpClient();
+    }
+    private void Start()
+    {
+
+    }
+
+    public void Send()
+    {
+        SendValue(System.Text.Encoding.Default.GetBytes("谭伟照傻傻嗨"));
+    }
+    //发送消息
+    public void SendValue(byte[] msg)
+    {
+        try
+        {
+            if (msg != null)
+            {
+                client.Send(msg, msg.Length, remoteEndPoint);
+            }
+        }
+        catch (Exception err)
+        {
+            Debug.LogError("Error udp send : " + err.Message);
+        }
+    }
+
+
+
+
+}
+
